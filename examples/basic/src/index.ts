c5abd6faa211c90e176e667cb39847afa781d2b6
Hugo Zonderland <hugo@zonder.land>
2025-05-31 16:13:34 +0200

    feat: added a proper basic demo, this will help developers understand how to use the client.

--- examples/basic/src/index.ts
+++ examples/basic/src/index.ts
@@ -1,25 +1,6 @@
-import { VirtualDisplayClient, State } from '@virtual-display/client';
+import { VirtualDisplayClient } from '../../../core/src';
 
-const client = new VirtualDisplayClient('#virtual-display');
-const state: State = {
-  attributes: [
-    {
-      name: 'Color',
-      values: [
-        { value: 'Red', identifiers: ['color-red'], isSelected: true },
-        { value: 'Blue', identifiers: ['color-blue'], isSelected: false },
-      ],
-    },
-    {
-      name: 'Size',
-      values: [
-        { value: 'Small', identifiers: ['small'], isSelected: false },
-        { value: 'Large', identifiers: ['large'], isSelected: true },
-      ],
-    },
-  ],
-};
-client.sendClientState(state);
+new VirtualDisplayClient('#virtual-display');
 
 function updateStatus(message: string): void {
   const statusElement = document.getElementById('status');
@@ -35,9 +16,12 @@ window.addEventListener('load', () => {
     const iframe = document.getElementById(
       'virtual-display'
     ) as HTMLIFrameElement;
+
     if (!iframe || !iframe.contentWindow) {
       throw new Error('Iframe not found or not ready');
     }
+
+    updateStatus('Iframe found! Displaying model.');
   } catch (error) {
     console.error('Initialization error:', error);
     updateStatus('Initialization failed');
